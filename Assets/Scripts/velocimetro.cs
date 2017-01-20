ff2d7183abfa0693dc9c8d383fe6357891dc4e17
Estudiante <wuiil94@hotmail.com>
2017-01-19 21:06:06 -0500

    se agrego velocimetro

---
+++ Assets/Scripts/velocimetro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class velocimetro : MonoBehaviour {
+	public Rigidbody objeto;
+	public Text texto;
+	float km;
+	void Start(){
+		objeto = GetComponent<Rigidbody> ();
+	}
+	void Update(){
+		km=objeto.velocity.magnitude*3.6f;
+		texto.text = "Km/h: " +Mathf.FloorToInt (km);
+	}
+}
